8e92638f82a4eee6e27a8dad0031a7e329a49e51
rashedrk <rashedrk001@gmail.com>
2023-11-19 23:38:15 +0600

    Asynchronous typescript

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -2,9 +2,13 @@
     //Promise
 
     //simulate
-    const createPromise = (): Promise<string> => {
-        return new Promise<string> ((resolve, reject) => {
-            const data: string = 'something';
+    type Something = { something: string };
+
+    const createPromise = (): Promise<Something> => {
+        return new Promise<Something>((resolve, reject) => {
+            const data: Something = {
+                something: 'something'
+            };
             if (data) {
                 resolve(data);
             }
@@ -14,12 +18,27 @@
         });
     }
     //calling createPromise
-    const showData = async(): Promise<string> => {
-        const data: string = await createPromise();
+    const showData = async (): Promise<Something> => {
+        const data: Something = await createPromise();
         // console.log(data);
         return data;
     }
-
     showData();
+
+    //external data 
+    type ToDo = {
+        userId: number;
+        id: number;
+        title: string;
+        completed: boolean;
+    }
+    const getTodo = async (): Promise <ToDo> => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+
+    getTodo();
     //
 }
