8ccfcf5e99b8a5f38fa57cdd2cc9eae6f5697f1e
rashedrk <rashedrk001@gmail.com>
2023-11-25 23:40:30 +0600

    Class with parameter property

--- module3/3.1.ts
+++ module3/3.1.ts
@@ -1,19 +1,24 @@
 {
     // OOP - Class
     class Animal {
-        name: string;
-        species: string;
-        sounds: string;
+        // name: string;
+        // species: string;
+        // sounds: string;
 
-        constructor(name: string, species: string, sounds: string){
-            this.name = name;
-            this.species = species;
-            this.sounds = sounds;
-        };
+        // constructor(name: string, species: string, sounds: string){
+        //     this.name = name;
+        //     this.species = species;
+        //     this.sounds = sounds;
+        // };
+
+        /*---------------------
+            Parameter Property
+         ---------------------- */
+        constructor(public name: string, public species: string, public sounds: string){}
 
         makeSounds() {
             console.log(`The ${this.name} says: ${this.sounds}`);
-            
+
         }
     }
 
