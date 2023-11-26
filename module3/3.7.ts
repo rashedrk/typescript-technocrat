36324f7b88663977d40e8f6bc438f1ef88bed4c8
rashedrk <rashedrk001@gmail.com>
2023-11-26 14:01:12 +0600

    OOP - Static

---
+++ module3/3.7.ts
@@ -0,0 +1,28 @@
+{
+    //Static
+    class Counter {
+        static count: number = 0;
+
+        increment(){
+            return Counter.count = Counter.count + 1;
+        }
+        decrement(){
+            return Counter.count = Counter.count - 1;
+        }
+        static multiply (){
+            return Counter.count = Counter.count * 2;
+        }
+    }
+
+    const instance1 = new Counter();
+    console.log(instance1.increment());
+    console.log(instance1.increment());
+    
+    const instance2 = new Counter();
+    console.log(instance2.increment());
+    
+    console.log(Counter.multiply());
+    
+
+    //
+}
